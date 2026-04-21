496fe8ce7ed22d0dceed6f201188bdfa3d50569a
francesco-troise <francesco.troise98@gmail.com>
2026-04-21 18:33:47 +0200

    Install Breeze and Bootstrap
    Check login functionality
    
    Create layout for admin area (modified dashboard)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
